11a891ed16863475f6c54bbd6c600694ea21f821
Flashspeed <flashspeed@hotmail.co.uk>
2019-02-27 01:21:03 +0000

    Fixed jar file

--- src/main/java/FormVideoDownloader.java
+++ src/main/java/FormVideoDownloader.java
@@ -1,5 +1,10 @@
 
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
+
 import javax.swing.*;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -7,27 +12,29 @@ import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class FormVideoDownloader {
-    private JTextField txtUrl;
-    private JComboBox cmbDownloadType;
-    private JPanel panelMain;
+public class FormVideoDownloader
+{
+    private JTextField        txtUrl;
+    private JComboBox         cmbDownloadType;
+    private JPanel            panelMain;
     private JComboBox<String> cmbAudioFormat;
-    private JButton btnDownloadLocation;
-    private JLabel lblDownloadLocation;
-    private JPanel panelOptions;
-    private JButton downloadButton;
-    private JLabel lblDownloadProgress;
-    private boolean isVideoDownloadSelected;
-    private JFileChooser jFileChooserForDownloadLocation = new JFileChooser();
-    private String saveDirectory = "";
-    private String operatingSystem;
-    private StringBuilder commandStringBuilder = new StringBuilder();
-    private String startingCommand = "cmd.exe /c youtube-dl ";
-    private String audioFormat; // e.g mp3, wav
-    private String videoFormat; // e.g mp4, mov, flv
+    private JButton           btnDownloadLocation;
+    private JLabel            lblDownloadLocation;
+    private JPanel            panelOptions;
+    private JButton           downloadButton;
+    private JLabel            lblDownloadProgress;
+    private boolean           isVideoDownloadSelected;
+    private JFileChooser      jFileChooserForDownloadLocation = new JFileChooser();
+    private String            saveDirectory                   = "";
+    private String            operatingSystem;
+    private StringBuilder     commandStringBuilder            = new StringBuilder();
+    private String            startingCommand                 = "cmd.exe /c youtube-dl ";
+    private String            audioFormat; // e.g mp3, wav
+    private String            videoFormat; // e.g mp4, mov, flv
 
     public FormVideoDownloader()
     {
+        $$$setupUI$$$();
         if (cmbDownloadType.getSelectedIndex() == 0)
         {
             cmbAudioFormat.setEnabled(false);
@@ -44,13 +51,13 @@ public class FormVideoDownloader {
         jFileChooserForDownloadLocation.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 
         cmbAudioFormat.addActionListener(e -> {
-            JComboBox jComboBox = (JComboBox)e.getSource();
+            JComboBox jComboBox = (JComboBox) e.getSource();
             audioFormat = jComboBox.getSelectedItem().toString();
             System.out.println(audioFormat);
         });
 
         cmbDownloadType.addActionListener(e -> {
-            JComboBox jComboBox = (JComboBox)e.getSource();
+            JComboBox jComboBox = (JComboBox) e.getSource();
             if (jComboBox.getSelectedIndex() == 0)
             {
                 cmbAudioFormat.setEnabled(false);
@@ -120,17 +127,17 @@ public class FormVideoDownloader {
 
                 }
 
-                if(!isVideoDownloadSelected)
+                if (!isVideoDownloadSelected)
                 {
                     commandStringBuilder.append("-x ")
-                    .append("--audio-format ").append(audioFormat).append(" ");
+                            .append("--audio-format ").append(audioFormat).append(" ");
                 }
 
-                if(!saveDirectory.isEmpty())
+                if (!saveDirectory.isEmpty())
                 {
                     commandStringBuilder.append("-o \"").append(saveDirectory).append("\" ")
-                    .append("--ignore-config ")
-                    .append("-f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' ");
+                            .append("--ignore-config ")
+                            .append("-f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' ");
                 }
 
                 // Final value in the command should be the url
@@ -140,15 +147,16 @@ public class FormVideoDownloader {
                 }
 
                 new Thread(new CMDRunnable(commandStringBuilder.toString())).start();
-                System.out.println("Ran (" + commandStringBuilder.toString() + ")" );
+                System.out.println("Ran (" + commandStringBuilder.toString() + ")");
                 commandStringBuilder.delete(startingCommand.length(), commandStringBuilder.length());
-                System.out.println("After StringBuilder reset (" + commandStringBuilder.toString() + ")" );
+                System.out.println("After StringBuilder reset (" + commandStringBuilder.toString() + ")");
             }
         });
     }
 
     /**
      * Checks if a given string is in the format of a URL
+     *
      * @param url String to check
      * @return True if the string is in a URL format
      */
@@ -162,6 +170,7 @@ public class FormVideoDownloader {
 
     /**
      * Returns string value representing the download progress
+     *
      * @param str String to search for download percentage e.g "[download] 34.9% ..."
      * @return
      */
@@ -198,6 +207,7 @@ public class FormVideoDownloader {
 
     /**
      * Prints the output returned from a shell command if any
+     *
      * @param usingProcess Process to monitor for output
      */
     public void getOutputFromCommand(Process usingProcess)
@@ -233,6 +243,7 @@ public class FormVideoDownloader {
 
     /**
      * Checks what operating system the program is running on
+     *
      * @param oSNameToCheck Operating system name to check e.g "Windows"
      * @return True if the operating system name is the same as the name passed in
      */
@@ -243,15 +254,16 @@ public class FormVideoDownloader {
 
     public void load()
     {
-        JFrame frame = new JFrame( "Video Downloader" );
-        frame.setContentPane( new FormVideoDownloader().panelMain );
-        frame.setDefaultCloseOperation( WindowConstants.DISPOSE_ON_CLOSE );
+        JFrame frame = new JFrame("Video Downloader");
+        frame.setContentPane(new FormVideoDownloader().panelMain);
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         frame.pack();
-        frame.setLocationRelativeTo( null );
-        frame.setVisible( true );
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
-    private void createUIComponents() {
+    private void createUIComponents()
+    {
         String[] audioFormats = {"mp3", "wav", "m4a"};
         String[] downloadTypes = {"Video", "Audio"};
 
@@ -261,4 +273,107 @@ public class FormVideoDownloader {
         cmbDownloadType = new JComboBox<>(downloadTypes);
         cmbDownloadType.setSelectedIndex(0);
     }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$()
+    {
+        createUIComponents();
+        panelMain = new JPanel();
+        panelMain.setLayout(new GridLayoutManager(8, 4, new Insets(0, 0, 0, 0), -1, -1));
+        panelMain.setMinimumSize(new Dimension(-1, -1));
+        panelMain.setPreferredSize(new Dimension(600, 400));
+        txtUrl = new JTextField();
+        Font txtUrlFont = this.$$$getFont$$$(null, -1, 18, txtUrl.getFont());
+        if (txtUrlFont != null) txtUrl.setFont(txtUrlFont);
+        panelMain.add(txtUrl, new GridConstraints(1, 2, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, 40), null, 0, false));
+        final JPanel panel1 = new JPanel();
+        panel1.setLayout(new GridLayoutManager(2, 2, new Insets(0, 0, 0, 0), -1, -1));
+        panelMain.add(panel1, new GridConstraints(5, 2, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        Font cmbDownloadTypeFont = this.$$$getFont$$$(null, -1, 14, cmbDownloadType.getFont());
+        if (cmbDownloadTypeFont != null) cmbDownloadType.setFont(cmbDownloadTypeFont);
+        panel1.add(cmbDownloadType, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(-1, 40), null, 0, false));
+        Font cmbAudioFormatFont = this.$$$getFont$$$(null, -1, 14, cmbAudioFormat.getFont());
+        if (cmbAudioFormatFont != null) cmbAudioFormat.setFont(cmbAudioFormatFont);
+        panel1.add(cmbAudioFormat, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(-1, 40), null, 0, false));
+        panelOptions = new JPanel();
+        panelOptions.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        panelOptions.setOpaque(true);
+        panel1.add(panelOptions, new GridConstraints(1, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        btnDownloadLocation = new JButton();
+        Font btnDownloadLocationFont = this.$$$getFont$$$(null, -1, 14, btnDownloadLocation.getFont());
+        if (btnDownloadLocationFont != null) btnDownloadLocation.setFont(btnDownloadLocationFont);
+        btnDownloadLocation.setText("Choose download location");
+        panelMain.add(btnDownloadLocation, new GridConstraints(3, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(-1, 30), null, 0, false));
+        lblDownloadLocation = new JLabel();
+        lblDownloadLocation.setFocusTraversalPolicyProvider(true);
+        Font lblDownloadLocationFont = this.$$$getFont$$$(null, -1, 14, lblDownloadLocation.getFont());
+        if (lblDownloadLocationFont != null) lblDownloadLocation.setFont(lblDownloadLocationFont);
+        lblDownloadLocation.setText("");
+        panelMain.add(lblDownloadLocation, new GridConstraints(3, 3, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JPanel panel2 = new JPanel();
+        panel2.setLayout(new GridLayoutManager(2, 4, new Insets(0, 0, 0, 0), -1, -1));
+        panelMain.add(panel2, new GridConstraints(6, 2, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        downloadButton = new JButton();
+        Font downloadButtonFont = this.$$$getFont$$$(null, -1, 26, downloadButton.getFont());
+        if (downloadButtonFont != null) downloadButton.setFont(downloadButtonFont);
+        downloadButton.setText("Download");
+        panel2.add(downloadButton, new GridConstraints(1, 0, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(130, 60), null, 0, false));
+        final Spacer spacer1 = new Spacer();
+        panel2.add(spacer1, new GridConstraints(0, 0, 1, 4, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        final JPanel panel3 = new JPanel();
+        panel3.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        panel3.setFocusable(false);
+        panel2.add(panel3, new GridConstraints(1, 2, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        lblDownloadProgress = new JLabel();
+        Font lblDownloadProgressFont = this.$$$getFont$$$(null, -1, 24, lblDownloadProgress.getFont());
+        if (lblDownloadProgressFont != null) lblDownloadProgress.setFont(lblDownloadProgressFont);
+        lblDownloadProgress.setOpaque(false);
+        lblDownloadProgress.setRequestFocusEnabled(false);
+        lblDownloadProgress.setText("");
+        panel3.add(lblDownloadProgress, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label1 = new JLabel();
+        Font label1Font = this.$$$getFont$$$(null, -1, 14, label1.getFont());
+        if (label1Font != null) label1.setFont(label1Font);
+        label1.setText("URL");
+        panelMain.add(label1, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final Spacer spacer2 = new Spacer();
+        panelMain.add(spacer2, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, 1, 1, null, new Dimension(10, -1), null, 0, false));
+        final Spacer spacer3 = new Spacer();
+        panelMain.add(spacer3, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(-1, 30), null, 0, false));
+        final Spacer spacer4 = new Spacer();
+        panelMain.add(spacer4, new GridConstraints(7, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(-1, 10), null, 0, false));
+        final Spacer spacer5 = new Spacer();
+        panelMain.add(spacer5, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(-1, 20), null, 0, false));
+        final Spacer spacer6 = new Spacer();
+        panelMain.add(spacer6, new GridConstraints(4, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(-1, 20), null, 0, false));
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    private Font $$$getFont$$$(String fontName, int style, int size, Font currentFont)
+    {
+        if (currentFont == null) return null;
+        String resultName;
+        if (fontName == null) {resultName = currentFont.getName();}
+        else
+        {
+            Font testFont = new Font(fontName, Font.PLAIN, 10);
+            if (testFont.canDisplay('a') && testFont.canDisplay('1')) {resultName = fontName;}
+            else {resultName = currentFont.getName();}
+        }
+        return new Font(resultName, style >= 0 ? style : currentFont.getStyle(), size >= 0 ? size : currentFont.getSize());
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    public JComponent $$$getRootComponent$$$() { return panelMain; }
+
 }
